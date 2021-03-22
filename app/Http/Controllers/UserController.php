fcf2454d7b9acb4c33486789a02e9d6a2594616a
Ilya N <buddyforever.dev@gmail.com>
2021-03-22 15:50:35 -0300

    T415-fix syntax error in international locations page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2366,7 +2366,7 @@ class UserController extends Controller
                 $internationalLocations = DB::table('users')
                                 ->select(
                                     DB::raw("COUNT('country') as count"), 
-                                    DB::raw("MAX('country') as country"),
+                                    DB::raw("MAX('country') as country")
                                 )
                                 ->where('country', '!=', null)
                                 ->groupBy('country')
